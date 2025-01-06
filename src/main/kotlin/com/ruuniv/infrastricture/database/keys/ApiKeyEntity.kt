b30b2a8d059ccab20937eb26e44d2687e9fe4e48
반달곰수 <121839239+bandalgomsu@users.noreply.github.com>
2025-01-06 22:06:57 +0900

    feat : Implement ApiKey Filter

--- src/main/kotlin/com/ruuniv/infrastricture/database/keys/ApiKeyEntity.kt
+++ src/main/kotlin/com/ruuniv/infrastricture/database/keys/ApiKeyEntity.kt
@@ -2,12 +2,15 @@ package com.ruuniv.infrastricture.database.keys
 
 import com.ruuniv.infrastricture.database.BaseEntity
 import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("api_key")
 class ApiKeyEntity(
     @Id
     var id: Long? = null,
+    @Column("api_key")
     var apiKey: String,
+    @Column("user_id")
     var userId: Long,
 ) : BaseEntity() {}
