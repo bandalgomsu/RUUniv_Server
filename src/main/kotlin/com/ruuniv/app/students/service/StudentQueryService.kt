f730e0ef4917c6c7b4528c3bdaa1d3c8d25f2ec0
반달곰수 <121839239+bandalgomsu@users.noreply.github.com>
2025-01-12 21:10:41 +0900

    feat : implement students by ApiKey Cache

--- src/main/kotlin/com/ruuniv/app/students/service/StudentQueryService.kt
+++ src/main/kotlin/com/ruuniv/app/students/service/StudentQueryService.kt
@@ -3,6 +3,8 @@ package com.ruuniv.app.students.service
 import com.ruuniv.app.students.dto.StudentQueryResponse
 import com.ruuniv.app.students.implement.StudentReader
 import kotlinx.coroutines.coroutineScope
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.cache.annotation.Caching
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,6 +12,11 @@ class StudentQueryService(
     private val studentReader: StudentReader,
 ) {
 
+    @Caching(
+        cacheable = [
+            Cacheable(cacheManager = "redisCacheManager", value = ["STUDENTS_BY_API_KEY"], key = "#apiKeyId"),
+        ]
+    )
     suspend fun getStudentsByApiKeyId(apiKeyId: Long): StudentQueryResponse.StudentsInfo = coroutineScope {
         val students = studentReader.readAll(apiKeyId).map {
             StudentQueryResponse.StudentInfo(
