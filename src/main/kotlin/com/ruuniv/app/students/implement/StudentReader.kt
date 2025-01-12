80938e3194b26f7e72230d6fe0c733e65b3b1d6c
반달곰수 <121839239+bandalgomsu@users.noreply.github.com>
2025-01-12 21:06:43 +0900

    feat : implement student readAll by ApiKeyId

--- src/main/kotlin/com/ruuniv/app/students/implement/StudentReader.kt
+++ src/main/kotlin/com/ruuniv/app/students/implement/StudentReader.kt
@@ -1,11 +1,16 @@
 package com.ruuniv.app.students.implement
 
 import com.ruuniv.app.students.dao.StudentDao
+import com.ruuniv.app.students.model.Student
+import kotlinx.coroutines.coroutineScope
 import org.springframework.stereotype.Component
 
 @Component
 class StudentReader(
-    private val repository: StudentDao
+    private val studentDao: StudentDao
 ) {
 
+    suspend fun readAll(apiKeyId: Long): List<Student> = coroutineScope {
+        studentDao.readAllByApiKeyId(apiKeyId)
+    }
 }
