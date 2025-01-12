10797fe22365d63fe6a28cc96e77be179eb29e1e
반달곰수 <121839239+bandalgomsu@users.noreply.github.com>
2025-01-12 20:37:58 +0900

    feat : Implement Cache

--- src/main/kotlin/com/ruuniv/app/keys/service/ApiKeyCommandService.kt
+++ src/main/kotlin/com/ruuniv/app/keys/service/ApiKeyCommandService.kt
@@ -3,6 +3,8 @@ package com.ruuniv.app.keys.service
 import com.ruuniv.app.keys.implement.ApiKeyWriter
 import com.ruuniv.common.util.UUIDGenerator
 import kotlinx.coroutines.coroutineScope
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.Caching
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -12,11 +14,21 @@ class ApiKeyCommandService(
 ) {
 
     @Transactional
+    @Caching(
+        evict = [
+            CacheEvict(cacheManager = "redisCacheManager", value = ["API_KEY"], key = "#userId"),
+        ]
+    )
     suspend fun createApiKey(apiKey: String = UUIDGenerator.generateUUID(), userId: Long) = coroutineScope {
         apiKeyWriter.add(apiKey, userId)
     }
 
     @Transactional
+    @Caching(
+        evict = [
+            CacheEvict(cacheManager = "redisCacheManager", value = ["API_KEY"], key = "#userId"),
+        ]
+    )
     suspend fun deleteApiKey(apiKeyId: Long, userId: Long) = coroutineScope {
         apiKeyWriter.delete(apiKeyId, userId)
     }
--- src/main/kotlin/com/ruuniv/app/keys/service/ApiKeyQueryService.kt
+++ src/main/kotlin/com/ruuniv/app/keys/service/ApiKeyQueryService.kt
@@ -3,6 +3,8 @@ package com.ruuniv.app.keys.service
 import com.ruuniv.app.keys.dto.ApiKeyQueryResponse
 import com.ruuniv.app.keys.implement.ApiKeyReader
 import kotlinx.coroutines.coroutineScope
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.cache.annotation.Caching
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,6 +12,11 @@ class ApiKeyQueryService(
     private val apiKeyReader: ApiKeyReader,
 ) {
 
+    @Caching(
+        cacheable = [
+            Cacheable(cacheManager = "redisCacheManager", value = ["API_KEY"], key = "#userId"),
+        ]
+    )
     suspend fun getApiKeysByUserId(userId: Long): ApiKeyQueryResponse.ApiKeysInfo = coroutineScope {
         val apiKeysInfo = apiKeyReader.readAll(userId).map {
             ApiKeyQueryResponse.ApiKeyInfo(
